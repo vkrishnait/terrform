acacdc53d9000b8b9935108c5d767258b06aaf78
I304224 <krishna.venkatesan@sap.com>
2021-04-02 09:50:05 +0530

    modified:   1.tf

--- 1.tf
+++ 1.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "ap-south-1a"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "krishna" {
     ami = "ami-0c55b159cbfafe1f0"
      instance_type = "t2.micro"
      host_id = "KV"
-     
+
 
 }
